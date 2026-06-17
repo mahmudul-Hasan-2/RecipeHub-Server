778781ed512870dcd66fd82c14680c440bebae52
mahmudul-Hasan-2 <mahmudul5790@gmail.com>
2026-06-17 22:10:28 +0600

    Already finish setup mongodb and already deployed to vercel

---
+++ index.js
@@ -0,0 +1,53 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Recipehub Server!", status: "success" });
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    app.get("/greetings", async (req, res) => {
+      res.json({ message: "Hello World!", status: "success" });
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
